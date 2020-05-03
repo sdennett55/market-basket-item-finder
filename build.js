45a735bae0ee73a2dddc36634cdd7699444a438d
sdennett55@gmail.com <sdennett55@gmail.com>
2020-05-03 18:09:37 -0400

    fixed build with babel

--- build.js
+++ build.js
@@ -2,6 +2,6 @@ var UglifyJS = require('uglify-js');
 var fs = require('fs');
 var path = require('path');
 
-const srcFile = fs.readFileSync(path.join('src', 'script.js'), "utf8");
+const srcFile = fs.readFileSync(path.join('dist', 'script-transpiled.js'), "utf8");
 
 fs.writeFileSync(path.join('dist', 'script.min.js'), UglifyJS.minify(srcFile).code, "utf8");
