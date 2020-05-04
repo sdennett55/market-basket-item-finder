94309998ec2d649b8479515fe235dd1309bf69f6
sdennett55@gmail.com <sdennett55@gmail.com>
2020-05-04 11:20:21 -0400

    adding index.html to dist folder for netlify

--- build.js
+++ build.js
@@ -5,3 +5,9 @@ var path = require('path');
 const srcFile = fs.readFileSync(path.join('dist', 'script-transpiled.js'), "utf8");
 
 fs.writeFileSync(path.join('dist', 'script.min.js'), UglifyJS.minify(srcFile).code, "utf8");
+
+const indexFile = fs.readFileSync('index.html', "utf8");
+
+console.log(indexFile);
+
+fs.writeFileSync(path.join('dist', 'index.html'), indexFile, "utf8");
